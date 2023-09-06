f4797b4ef1f4df1e5b5fd3eff18fea0900bec804
Rishabh Jaiswal <97353246+rishabhjaiswaliit@users.noreply.github.com>
2023-09-06 20:36:53 +0530

    Create 002_inhertiance.py

---
+++ 002_inhertiance.py
@@ -0,0 +1,40 @@
+class Vehicle:  # defining the parent class
+    fuelCap = 90
+
+
+class Car(Vehicle):  # defining the child class
+    fuelCap = 50
+
+    def display(self):
+        # accessing fuelCap from the Vehicle class using super()
+        print("Fuel cap from the Vehicle Class:", super().fuelCap)
+
+        # accessing fuelCap from the Car class using self
+        print("Fuel cap from the Car Class:", self.fuelCap)
+
+
+obj1 = Car()  # creating a car object
+obj1.display()  # calling the Car class method display()
+
+
+# -----------------
+
+
+class ParentClass():
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+
+class ChildClass(ParentClass):
+    def __init__(self, a, b, c): 
+        # same as
+        ParentClass.__init__(self, a, b)
+        super().__init__(a, b) 
+        self.c = c
+
+
+obj = ChildClass(1, 2, 3)
+print(obj.a)
+print(obj.b)
+print(obj.c)
