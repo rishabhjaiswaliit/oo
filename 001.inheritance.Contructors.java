aaad749f641b31a1a4fe8b238419974ce6624786
Rishabh Jaiswal <97353246+rishabhjaiswaliit@users.noreply.github.com>
2023-09-07 23:16:43 +0530

    Create 001.inheritance.Contructors.java

---
+++ 001.inheritance.Contructors.java
@@ -0,0 +1,34 @@
+public class YourClassNameHere {
+    public static void main(String[] args) {
+      Child c = new Child();
+    }
+}
+
+class Parent {
+  int a;
+  int b;
+  Parent() {
+    this(10);
+    System.out.println("2");
+  }
+  Parent(int a) {
+    System.out.println("1");
+  }
+}
+
+class Child extends Parent {
+  int c;
+  Child()  {
+    this(10);
+    System.out.println("5");
+    
+  }
+  Child(int c) {
+    this(10, 10, 10);
+    System.out.println("4");
+  }
+  Child(int a, int b, int c) {
+    super();
+    System.out.println("3");
+  }
+}
