0f0efa29aa7a7e43c6a1fdb1aa2f80a62cef7a94
Rishabh Jaiswal <97353246+rishabhjaiswaliit@users.noreply.github.com>
2023-09-14 22:49:14 +0530

    Create 6.pq.java

---
+++ interviewbit/6.pq.java
@@ -0,0 +1,29 @@
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Queue<Integer> q = new PriorityQueue<>();
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            
+            if (x == 1) {
+                q.add(y);
+            } else if (x == 2) {
+                Integer peekedVal = q.peek();
+                System.out.println(peekedVal != null ? peekedVal: -1);
+            } else if (x == 3) {
+                 q.poll();
+            }
+        }
+
+    }
+}
