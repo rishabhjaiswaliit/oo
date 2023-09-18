527eb9163bf77c12380394609ad7d9a87f0589d5
Rishabh Jaiswal <97353246+rishabhjaiswaliit@users.noreply.github.com>
2023-09-18 23:51:45 +0530

    Create 008WRONG.shallowCopy.java

---
+++ 008WRONG.shallowCopy.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Vector v1 = new Vector(10, 20);
+            v1.arr[9] = "SOMETHING";
+            Vector v2 = v1.clone();
+            v2.arr[0] = "new";          
+            System.out.println(v1.arr[0]);
+        } catch (CloneNotSupportedException e) {
+            System.out.println("dd");
+        }
+    }
+
+}
+class Vector implements Cloneable {
+    int x;
+    int y;
+    String[] arr = new String[10];
+    public Vector(int x, int y) {
+        System.out.println("constructor HERE");
+        this.x = x;
+        this.y = y;
+        this.arr[0] = "old";
+    }
+    public Vector() {}
+    public Vector clone() throws CloneNotSupportedException {
+      return (Vector) super.clone();
+    }
+}
