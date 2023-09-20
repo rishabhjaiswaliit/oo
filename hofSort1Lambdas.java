eee85f395f9268108affa4585d7c94eedab76984
Rishabh Jaiswal <97353246+rishabhjaiswaliit@users.noreply.github.com>
2023-09-20 23:23:13 +0530

    Update hofSort1Lambdas.java

--- hofSort1Lambdas.java
+++ hofSort1Lambdas.java
@@ -10,3 +10,19 @@ public class Main {
       Arrays.sort(strings, (a, b) -> b.length() - a.length());
   }
 }
+
+
+// from functools import cmp_to_key
+// arr = [9, 8, -1, 7]
+// arr.sort(key=cmp_to_key(lambda a, b: b - a ))
+
+// strings = ['1', '2', '10']
+
+// strings.sort(key=cmp_to_key(lambda a, b: int(b) - int(a)))
+// strings.sort(key=cmp_to_key(lambda a, b: -1 if b < a else (0 if b == a else 1) ))
+// strings.sort(key=cmp_to_key(lambda a, b: len(b) - len(a)))
+
+// # shortcuts
+// strings.sort(key=lambda a: int(a), reverse=True)
+// strings.sort(reverse=True)
+// strings.sort(key=lambda a: len(a), reverse=True)
