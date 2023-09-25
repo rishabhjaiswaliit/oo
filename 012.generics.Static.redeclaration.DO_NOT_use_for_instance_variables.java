99971d34a49fff2fa3ca83025e9dc6b7b6eae8ac
Rishabh Jaiswal <97353246+rishabhjaiswaliit@users.noreply.github.com>
2023-09-25 23:00:35 +0530

    Create 012.generics.Static.redeclaration.DO_NOT_use_for_instance_variables.java

---
+++ 012.generics.Static.redeclaration.DO_NOT_use_for_instance_variables.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        print("String");
+        print(false);
+        print(12345);
+        print(12.3);
+        print(new Object());
+    }
+
+    static <T> void print(T obj) {
+        System.out.println(obj);
+    }
+}
+
